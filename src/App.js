ef1c03fb01a3807e49c68223a9fed541aa78225d
SubhamRaoniar28 <41485604+SubhamRaoniar28@users.noreply.github.com>
2021-02-23 19:35:02 +0530

    added white and black color for user and guest_user, before deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import { FormControl, IconButton, Input } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import './App.css';
 import db from './firebase';
 import Message from './Message';
 import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
 
 function App() {
 
@@ -43,14 +44,16 @@ function App() {
 //in above it's returning a <p><p/> tag
   return (
     <div className="App">
-      <h1>Hello FbM</h1>
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
+      <h1>Facebook Messenger</h1>
       <h2>Welcome {username}</h2>
 
-      <form>
-        <FormControl>
-          <InputLabel>Enter a message...</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)} />
-          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input className="app__input" placeholder="Enter a message..." value={input} onChange={event => setInput(event.target.value)} />
+          <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>
+            <SendIcon />
+          </IconButton>
         </FormControl>       
       </form>
 
--- src/Message.js
+++ src/Message.js
@@ -5,15 +5,13 @@ import './Message.css';
 const Message = forwardRef(({ message, username }, ref) => {
 
     const isUser = username === message.username;
-    console.log('loginUser : ' + username + ' isUser: ' +  isUser);
-    console.log('msgUser : ' + message.username + ' msgTecx : ' + message.text);
 
     return (
         <div ref={ref} className={`message ${isUser && 'message__user'}`}>
             <Card className={isUser ? "message__userCard" : "message_guestCard"}>
                 <CardContent>
-                    <Typography color="white" variant="h5" component="h2">
-                        {message.username}: {message.message}
+                    <Typography className={isUser ? "user__typography" : "guest_typography"} color="white" variant="h5" component="h2">
+                        {!isUser && `${message.username || 'Unknown User'}: `} {message.message}
                     </Typography>
                 </CardContent>
             </Card>
