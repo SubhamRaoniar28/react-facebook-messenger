930517c9f3dd9f56fecc945daf9837db5a4e966b
SubhamRaoniar28 <41485604+SubhamRaoniar28@users.noreply.github.com>
2021-02-23 22:10:24 +0530

    Some final touch

--- src/Message.js
+++ src/Message.js
@@ -8,10 +8,11 @@ const Message = forwardRef(({ message, username }, ref) => {
 
     return (
         <div ref={ref} className={`message ${isUser && 'message__user'}`}>
+            <p>{!isUser && `${message.username || 'Unknown User'}`}</p>
             <Card className={isUser ? "message__userCard" : "message_guestCard"}>
-                <CardContent>
-                    <Typography className={isUser ? "user__typography" : "guest_typography"} color="white" variant="h5" component="h2">
-                        {!isUser && `${message.username || 'Unknown User'}: `} {message.message}
+                <CardContent>   
+                    <Typography className={isUser ? "user__typography" : "guest_typography"} color="white" variant="h5" component="h2">                    
+                         {message.message}
                     </Typography>
                 </CardContent>
             </Card>
