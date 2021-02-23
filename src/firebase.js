0305eb9969ed83074e7942533ae611957268800e
SubhamRaoniar28 <41485604+SubhamRaoniar28@users.noreply.github.com>
2021-02-23 22:21:11 +0530

    Final touch

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
 
-    apiKey: "AIzaSyBjO0TP0ypyyySNwtNNnASdyxWFAvLZaMA",
-    authDomain: "facebook-messenger-8b111.firebaseapp.com",
-    projectId: "facebook-messenger-8b111",
-    storageBucket: "facebook-messenger-8b111.appspot.com",
-    messagingSenderId: "876410151029",
-    appId: "1:876410151029:web:2a41388aaaa9a78f7b983c",
-    measurementId: "G-BRC31M77D9"
+    apiKey: "YOUR_API_KEY",
+    AuthDomain: "YOUR_PROJECT_AUTH_DOMAIN",
+    databaseURL: "YOUR_PROJECT_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_PROJECT_MESSAGING_SENDER_ID",
+    appId: "YOUR_PROJECT_APP_ID"
 
 });
 
